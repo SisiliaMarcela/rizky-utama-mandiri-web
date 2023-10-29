359e6059d81a7c1f21c8d2b941faca1172ac9006
SisiliaMarcela <sisiliamarc@gmail.com>
2023-10-29 17:43:51 +0700

    spatie roles

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,16 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+use App\Filament\Resources\EmployeeResource;
+use App\Filament\Resources\ExcavatorResource;
+use App\Filament\Resources\OperationalCostResource;
+use App\Filament\Resources\SparepartResource;
+use App\Models\Employee;
+use Filament\Navigation\NavigationBuilder;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
+use Filament\Pages\Dashboard;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -53,6 +63,51 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return $builder->groups([
+                    NavigationGroup::make()
+                        ->items([
+                            NavigationItem::make('Dashboard')
+                                ->icon('heroicon-o-home')
+                                ->isActiveWhen(fn (): bool => request()->routeIs('filament.admin.pages.dashboard'))
+                                ->url(fn (): string => Dashboard::getUrl()),
+                        ]),
+                    NavigationGroup::make('Setting')->items([
+                        NavigationItem::make('Roles')
+                            ->icon('heroicon-o-user-group')
+                            ->isActiveWhen(fn (): bool => request()->routeIs([
+                                'filament.admin.resources.roles.index',
+                                'filament.admin.resources.roles.create',
+                                'filament.admin.resources.roles.view',
+                                'filament.admin.resources.roles.edit',
+                            ]))
+                            ->url(fn (): string => '/admin/roles'),
+                        NavigationItem::make('Permissions')
+                            ->icon('heroicon-o-lock-closed')
+                            ->isActiveWhen(fn (): bool => request()->routeIs([
+                                'filament.admin.resources.permissions.index',
+                                'filament.admin.resources.permissions.create',
+                                'filament.admin.resources.permissions.view',
+                                'filament.admin.resources.permissions.edit',
+                            ]))
+                            ->url(fn (): string => '/admin/permissions'),
+                    ]),
+                    NavigationGroup::make('Excavator')
+                        ->items([
+                            ...ExcavatorResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Sparepart')
+                        ->items([
+                            ...SparepartResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Workshop')
+                        ->items([
+                            ...EmployeeResource::getNavigationItems(),
+                            ...OperationalCostResource::getNavigationItems(),
+                        ]),
+                ]);
+            });
     }
 }
